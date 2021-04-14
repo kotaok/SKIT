9c0ed72c0d165764d3b1b2490b290503966b05f3
kotaok <kota813e@gmail.com>
2021-04-14 12:58:51 +0900

    ベース画面の作成

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,57 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-container>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              chat work社
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense nav>
+      <v-list-item v-for="menu_list in menu_lists" :key="menu_list.name">
+        <v-list-item-icon>
+          <v-icon>{{ menu_list.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{ menu_list.name }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      </v-list>
+      </v-container>
+    </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>chat work</v-toolbar-title>
+    </v-app-bar>  
 
-      <v-spacer></v-spacer>
+    <v-footer color="primary" dark app>
+      chat work
+    </v-footer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
 
   components: {
-    HelloWorld,
+    
   },
 
   data: () => ({
-    //
+    drawer: null,
+    menu_lists:[
+      {name: '開発部署',icon: 'mdi-lock'},
+      {name: 'デザイン部署',icon: 'mdi-lock'},
+      {name: 'お知らせ',icon: 'mdi-pound'},
+      {name: 'ヘルプ',icon: 'mdi-pound'},
+    ]
   }),
 };
 </script>
