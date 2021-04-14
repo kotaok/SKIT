6d45bf5e20dcb2e61907a542d4e498fc9f84370f
kotaok <kota813e@gmail.com>
2021-04-14 13:18:00 +0900

    firebaseの導入

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore'
 import './registerServiceWorker'
 
 const vuetify = process.env.NODE_ENV === "production"
@@ -8,6 +11,20 @@ const vuetify = process.env.NODE_ENV === "production"
   : require('./plugins/vuetify-dev').default;
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDscClB0Ixn1Xd3TpG4rPmZJ91JCXmKNfY",
+  authDomain: "chat-work-app.firebaseapp.com",
+  databaseURL: "https://chat-work-app-default-rtdb.firebaseio.com",
+  projectId: "chat-work-app",
+  storageBucket: "chat-work-app.appspot.com",
+  messagingSenderId: "344447311983",
+  appId: "1:344447311983:web:c2cd12f0537ca5313f6d9a",
+  measurementId: "G-4W6FPCF7R1"
+};
+
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
